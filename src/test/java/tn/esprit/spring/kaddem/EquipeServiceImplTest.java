d5e616e905b51d1f55fa805d688646592e72d4b3
houri anis <hourianis50@gmail.com>
2024-11-07 02:41:21 +0100

    testttfffttyyyppyyttttiirrtttoo

--- src/test/java/tn/esprit/spring/kaddem/EquipeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/EquipeServiceImplTest.java
@@ -13,6 +13,7 @@ import javax.persistence.EntityNotFoundException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
